9115b7fa7b06cce8207aebd1df5f706f72fe041b
RajaSri1218 <88514634+RajaSri1218@users.noreply.github.com>
2021-08-06 03:58:42 +0530

    Add files via upload

---
+++ src/events.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 0,
+    title: "DSM Call",
+    allDay: false,
+    start: new Date(2021, 7, 6,12,30,0),
+    end: new Date(2021, 7, 6, 13, 0, 0)
+  }, 
+  {
+    id: 1,
+    title: "Sprint Call",
+    allDay: false,
+    start: new Date(2021, 7, 30,12,30,0),
+    end: new Date(2021, 7, 30, 13, 0, 0)
+  }, 
+];
