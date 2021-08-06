241746f7f7dbfa12965d628a2b982e4c7976517e
RajaSri1218 <88514634+RajaSri1218@users.noreply.github.com>
2021-08-06 09:25:42 +0530

    Update events.js

--- src/events.js
+++ src/events.js
@@ -1,16 +1,178 @@
 export default [
   {
     id: 0,
-    title: "DSM Call",
+    title: "DSM",
     allDay: false,
-    start: new Date(2021, 7, 6,12,30,0),
-    end: new Date(2021, 7, 6, 13, 0, 0)
-  }, 
+    start: new Date(2021 , 7 , 2,12,30,0),
+    end: new Date(2021, 7, 2,13,0,0)
+  },
   {
     id: 1,
-    title: "Sprint Call",
+    title: "DSM",
     allDay: false,
-    start: new Date(2021, 7, 30,12,30,0),
-    end: new Date(2021, 7, 30, 13, 0, 0)
-  }, 
+    start: new Date(2021 , 7 , 3,12,30,0),
+    end: new Date(2021, 7, 3,13,0,0)
+  },
+  {
+    id: 2,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 4,12,30,0),
+    end: new Date(2021, 7, 4,13,0,0)
+  },
+  {
+    id: 3,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 5,12,30,0),
+    end: new Date(2021, 7, 5,13,0,0)
+  },
+  {
+    id: 4,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 6,12,30,0),
+    end: new Date(2021, 7, 6,13,0,0)
+  },
+  {
+    id: 5,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 9,12,30,0),
+    end: new Date(2021, 7, 9,13,0,0)
+  },
+  {
+    id: 6,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 10,12,30,0),
+    end: new Date(2021, 7, 10,13,0,0)
+  },
+  {
+    id: 7,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 11,12,30,0),
+    end: new Date(2021, 7, 11,13,0,0)
+  },
+  {
+    id: 8,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 12,12,30,0),
+    end: new Date(2021, 7, 12,13,0,0)
+  },
+  {
+    id: 9,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 13,12,30,0),
+    end: new Date(2021, 7, 13,13,0,0)
+  },
+  {
+    id: 10,
+    title: "Weekly Sprint",
+    allDay: false,
+    start: new Date(2021 , 7 , 4,11,0,0),
+    end: new Date(2021, 7, 4,12,0,0)
+  },
+  {
+    id: 11,
+    title: "Weekly Sprint",
+    allDay: false,
+    start: new Date(2021 , 7 , 11,11,0,0),
+    end: new Date(2021, 7, 11,12,0,0)
+  },
+  {
+    id: 12,
+    title: "Monthly Sprint",
+    allday: false,
+    start: new Date(2021 , 7 , 11,15,0,0),
+    end: new Date(2021, 7, 11,16,0,0)
+  },
+  {
+    id: 13,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 16,12,30,0),
+    end: new Date(2021, 7, 16,13,0,0)
+  },
+  {
+    id: 14,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 17,12,30,0),
+    end: new Date(2021, 7, 17,13,0,0)
+  },
+  {
+    id: 15,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 18,12,30,0),
+    end: new Date(2021, 7, 18,13,0,0)
+  },
+  {
+    id: 16,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 19,12,30,0),
+    end: new Date(2021, 7, 19,13,0,0)
+  },
+  {
+    id: 17,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 20,12,30,0),
+    end: new Date(2021, 7, 20,13,0,0)
+  },
+  {
+    id: 18,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 23,12,30,0),
+    end: new Date(2021, 7, 23,13,0,0)
+  },
+  {
+    id: 19,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 24,12,30,0),
+    end: new Date(2021, 7, 24,13,0,0)
+  },
+  {
+    id: 20,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 25,12,30,0),
+    end: new Date(2021, 7, 25,13,0,0)
+  },
+  {
+    id: 21,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 26,12,30,0),
+    end: new Date(2021, 7, 26,13,0,0)
+  },
+  {
+    id: 22,
+    title: "DSM",
+    allDay: false,
+    start: new Date(2021 , 7 , 27,12,30,0),
+    end: new Date(2021, 7, 27,13,0,0)
+  },
+  {
+    id: 23,
+    title: "Weekly Sprint",
+    allDay: false,
+    start: new Date(2021 , 7 , 18,11,0,0),
+    end: new Date(2021, 7, 18,12,0,0)
+  },
+  {
+    id: 24,
+    title: "Weekly Sprint",
+    allDay: false,
+    start: new Date(2021 , 7 , 25,11,0,0),
+    end: new Date(2021, 7, 25,12,0,0)
+  }
+ 
 ];
